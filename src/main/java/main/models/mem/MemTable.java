a31f2362f7d4bda71704008c0d26ed2dad343f79
Sriparna Chakraborty <sriparna.c@flipkart.com>
2020-04-09 12:53:04 +0530

    memtable impl

--- src/main/java/main/models/mem/MemTable.java
+++ src/main/java/main/models/mem/MemTable.java
@@ -1,6 +1,7 @@
 package main.models.mem;
 
 import lombok.*;
+import main.impl.BST;
 import main.interfaces.CRUD;
 import main.interfaces.Initializable;
 
@@ -10,7 +11,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 
 public class MemTable implements Initializable, CRUD<String, String> {
-    @Getter private TreeMap<String, DataItem> data;
+    @Getter private BST<String, DataItem> data;
     @Getter @Setter private String walID;
     @Getter @Setter private AtomicInteger beginLogID;
     @Getter @Setter private AtomicInteger endLogID;
@@ -18,29 +19,30 @@ public class MemTable implements Initializable, CRUD<String, String> {
     @Override
     public void insert(String key, String value) {
         DataItem dataItem = DataItem.builder().rowID(key).value(value).isDeleted(false).build();
-        data.put(key, dataItem);
+        data.insert(dataItem);
     }
 
     @Override
     public void update(String key, String value) {
         DataItem dataItem = DataItem.builder().rowID(key).value(value).isDeleted(false).build();
-        data.put(key, dataItem);
+        data.insert(dataItem);
     }
 
     @Override
     public void delete(String key) {
         DataItem dataItem = DataItem.builder().rowID(key).value(null).isDeleted(true).build();
-        data.put(key, dataItem);
+        data.insert(dataItem);
     }
 
     @Override
     public String read(String key) {
-        return data.get(key).value;
+        DataItem dataItem = data.search(key);
+        return (dataItem == null) ? null : dataItem.value;
     }
 
     @Override
     public void initialize() throws Exception {
-        data = new TreeMap<>();
+        data = new BST<>(Comparator.comparing(String::toString));
     }
 
     @Override
@@ -49,21 +51,26 @@ public class MemTable implements Initializable, CRUD<String, String> {
     }
 
     public int count(){
-        return data.size();
+        return data.getCount();
     }
 
     public List<DataItem> getDataItemList(){
-        return new ArrayList<>(data.values());
+        return data.getAll();
     }
 
     @NoArgsConstructor
     @AllArgsConstructor
     @Data
     @Builder
-    public static final class DataItem{
+    public static final class DataItem implements BST.NodeItem<String>{
         private String rowID;
         private boolean isDeleted;
         private String value;
+
+        @Override
+        public String getID() {
+            return rowID;
+        }
     }
 
 }
